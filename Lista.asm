126a962651893f3179849df4893046173cbc7186
Arturo Yepez <arturyepez10@gmail.com>
2019-04-10 23:34:45 -0400

    se arregla el uso del contador de elementos de la lista y se modifica main_loop para que sea iterativo si se desea seguir añadiendo elementos

--- Lista.asm
+++ Lista.asm
@@ -1,17 +1,23 @@
 ### ESTRUCTURA DE DATOS: Lista Simplemente Enlazada
 
 .data
-	answer: .word
+	# Se declaran las palabras reservadas para evitar que se deban alinear
 	
+	answer: .word
 	Head: .word
-
+	
+	# Se declaran los mensajes necesarios para el usuario
+	
 	msg1: .asciiz "La lista no tiene ningún elemento."
 	msg2: .asciiz "¿Desea crear un nuevo nodo? Marque 1 para si y 0 para no"
 	msg3: .asciiz "Se ha creado un nuevo nodo"
 	msg_key: .asciiz "Elija el key de su elemento"
 	
+	menu1: .asciiz "¿Desea añadir otro nodo? Marque 1 para Si y 0 para No"
+	
+	prueba: .asciiz "La cantidad de elementos en la lista es: "
+	
 	collect_answer: .asciiz "Respuesta: "
-
 
 	salto_de_linea: .asciiz "\n"
 	
@@ -27,7 +33,6 @@
 		#Se crea nodo Cabeza/1er Nodo
 		jal Cabeza_Lista
 		
-		
 		#Al retornar $v0 contiene la dirección de espacio reservada,
 		#que procedemos a guardar datos en $sp
 		
@@ -35,12 +40,15 @@
 		sw $v0, ($sp)
 		#Movemos el apuntador de $sp
 		sub $sp, $sp, 4
-		#Se inicializa el contador de elementos de la lista en $t0
-		sub $t0, $zero, $zero
 		
 		
-		#Ahora debe comenzar un loop para añadir nodos añadiendo nodos
+		#Ahora debe comenzar un loop para añadir nodos
 		main_loop:
+			#Checa el tamaño del size de la lista, si es 0 crea nodo automaticamente
+			lw $a0, Head
+			lw $a0, 8($a0)
+			beqz $a0, Nodo
+			
 			#Se pregunta si desea añadir algún nodo
 			la $a0, msg2
 			jal print_string
@@ -53,16 +61,22 @@
 			li $v0, 5
 			syscall
 			
+			#Si el valor es 0 se sale del loop y pasa al menu
+			beqz $v0, main_menu
+			
 			jal newline
-			
-			#Movemos el valor de la respuesta a $a1
-			move $a1, $v0
-			
-			#Si el valor es 0 señala a la etiqueta de salida
-			beqz $a1, Salida
 			
 			#Si el valor es 1 procede a crear el nodo
 			jal Nodo
+			
+			#Se repite el loop para ver si se añade otro nodo
+			jal main_loop
+			
+			
+		main_menu:
+			
+			
+			
 			
 		
 
@@ -79,6 +93,12 @@ print_string:
 	syscall
 	jr $ra
 	
+# Imprimir Entero
+print_integer:
+	li $v0, 1
+	syscall
+	jr $ra
+	
 # Salto de linea
 newline:
 	li $v0, 4
@@ -90,14 +110,14 @@ newline:
 Cabeza_Lista:
 	#Se reserva el espacio para:
 	#Dirección Primer Nodo, Direción Ultimo Nodo, Tamaño de la lista
-	add $a0, $zero, 12
+	addi $a0, $zero, 12
 	li $v0, 9
 	syscall
 	
 	#Comenzamos el contador de Tamaño en 0
 	sw $zero, 8($v0)
 	
-	#Guardamos la dirección en memoria en la dirección Head para su facil acceso luego
+	#Guardamos la dirección reservada en la dirección de Head para su facil acceso luego
 	sw $v0, Head
 	
 	jr $ra
@@ -111,9 +131,10 @@ Nodo:
 	#----------
 	
 	#Sumamos 1 al size 
-	lw $a0, Head+8
+	lw $a1, Head
+	lw  $a0, 8($a1)
 	addi $a0, $a0, 1
-	sw $a0, Head+8	
+	sw $a0, 8($a1) 	
 	
 	#Se reserva espacio para:
 	#Elemento, Dirección del Siguiente
@@ -142,6 +163,8 @@ Nodo:
 	lw $a1, 4($sp)
 	sw $v0, ($a1)
 	
+	#AÑADIR CONECTAR CON EL SIGUIENTE NODO
+	
 	#----------
 	#Se recuperan las variables
 	addi $sp, $sp, 4
@@ -160,3 +183,18 @@ Nodo:
 		#Ejecuta la lectura del contenido de la dirección en $ao
 		li $v0, 1
 		syscall
+		
+	### UTIL PARA IMPRIMIR EL CONTADOR DE ELEMENTOS EN LA LISTA (A partir de la línea 137)
+			#nueva linea
+			jal newline
+			#carga a $a1 dirección donde está guardada Cabeza_Lista
+			lw $a1, Head
+			#dada el contenido de $a1 lo interpreta como dirección+8 y carga ese contenido en $a2 (ese contenido es el contador)
+			lw  $a2, 8($a1)
+			#carga en $a0 el mensaje de prueba y lo imprime
+			la $a0, prueba
+			jal print_string
+			#carga en $a0 el contenido de la dirección de $a2 y lo imprime
+			la $a0, ($a2)
+			jal print_integer
+			jal newline
